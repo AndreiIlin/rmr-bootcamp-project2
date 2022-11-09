bc035e200c9cbbf1cfaa5d3416477fa9215d62dc
AndreiIlin <andrei362065@gmail.com>
2022-11-09 16:39:45 +0300

    fix: Some bugs

--- src/features/auth/Registration/Registration.tsx
+++ src/features/auth/Registration/Registration.tsx
@@ -242,7 +242,7 @@ export const Registration = () => {
             {warningMessage}
           </FormHelperText>
         )}
-        <Button variant={'contained'} size={'large'} type={'submit'}>
+        <Button variant={'contained'} size={'large'} type={'submit'} sx={{ mt: 2 }}>
           Зарегистрироваться
         </Button>
         <Button size={'small'} onClick={() => goToLogin()}>
